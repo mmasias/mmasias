f12234c48d1f3f07911890d8df4157955651201f
mmasias@netbook <manuel@masiasweb.com>
2023-01-08 13:58:29 +0100

    Definición de zonas infranqueables (paredes, agua, etc)

--- PROGRAMACION-1/mapasConArrayAsociativo/ArrayAsociativo009.java
+++ PROGRAMACION-1/mapasConArrayAsociativo/ArrayAsociativo009.java
@@ -278,24 +278,19 @@ public class ArrayAsociativo009 {
 
         int[] elPersonaje = { 31, 33 };
 
-        String[] mundo;
-        mundo = castilloEX;
-        inicializaMundo(mundo, dosCastillos);
+        inicializarMundo(castilloLB);
 
         do {
             actualizarTiempo();
-            imprimirMundo(mundo, elPersonaje);
-            verAccion(elPersonaje, mundo);
+            imprimirMundo(castilloLB, elPersonaje);
+            verAccion(elPersonaje, castilloLB);
         } while (jugando);
-
     }
 
-    private static void inicializaMundo(String[] mundo, String[] unMapa) {
+    private static void inicializarMundo(String[] mundo) {
 
         viewPort = 7;
         alcanceVision = viewPort / 2;
-
-        mundo = unMapa;
 
         minFila = 0 + viewPort;
         minColumna = 0 + viewPort;
@@ -304,6 +299,7 @@ public class ArrayAsociativo009 {
     }
 
     static void actualizarTiempo() {
+
         hora = hora + 0.2;
         if (hora >= 24) {
             hora = 0;
@@ -326,16 +322,13 @@ public class ArrayAsociativo009 {
         } else if (hora < 6 || hora > 18) {
             alcanceVision = alcanceVision / 5 * 4;
         }
-
     }
 
     static void imprimirMundo(String[] castillo, int[] personaje) {
 
         String elemento;
         limpiarPantalla();
-        imprimirLinea();
         imprimirElCielo();
-        imprimirLinea();
         for (int fila = personaje[FILA] - viewPort; fila <= personaje[FILA] + viewPort; fila++) {
             for (int columna = personaje[COLUMNA] - viewPort; columna <= personaje[COLUMNA] + viewPort; columna++) {
 
@@ -347,20 +340,20 @@ public class ArrayAsociativo009 {
                 if (!(Math.pow((fila - personaje[FILA]), 2)+ Math.pow((columna - personaje[COLUMNA]), 2) <= alcanceVision * alcanceVision)) {
                     elemento = mapear('D', modoVisualizacion);
                 }
-
                 System.out.print(elemento);
             }
             System.out.println();
         }
-        imprimirLinea();
         imprimirStatus(personaje);
-        imprimirLinea();
     }
 
     private static void imprimirStatus(int[] personaje) {
+        
+        imprimirLinea();
         System.out.print("HORA: [" + (int) hora + "] ");
         System.out.print("/ (" + personaje[FILA] + "," + personaje[COLUMNA] + ")");
         System.out.println(" / SKIN[" + modoVisualizacion + "]");
+        imprimirLinea();
     }
 
     static void imprimirLinea() {
@@ -520,7 +513,6 @@ public class ArrayAsociativo009 {
         if (unPersonaje[COLUMNA] > maxColumna) {
             unPersonaje[COLUMNA] = unPersonaje[COLUMNA] - 1;
         }
-
     }
 
     static void verAccion(int[] elPersonaje, String[] elMundo) {
@@ -589,7 +581,8 @@ public class ArrayAsociativo009 {
     }
 
     static void imprimirElCielo() {
-
+        
+        imprimirLinea();
         for (int i = 0; i < viewPort * 2 + 1; i = i + 1) {
             if ((hora > 6) && (hora <= 18) && (i == (int) (((viewPort * 2)) - ((hora - 7) * (viewPort * 2) / 12)))) {
                 System.out.print(mapear('S', modoVisualizacion));
@@ -598,79 +591,71 @@ public class ArrayAsociativo009 {
             }
         }
         System.out.println();
+        imprimirLinea();
     }
 
-    // Reset
-    public static final String RESET = "\033[0m"; // Text Reset
+    public static final String RESET = "\033[0m";
 
-    // Regular Colors
-    public static final String BLACK = "\033[0;30m"; // BLACK
-    public static final String RED = "\033[0;31m"; // RED
-    public static final String GREEN = "\033[0;32m"; // GREEN
-    public static final String YELLOW = "\033[0;33m"; // YELLOW
-    public static final String BLUE = "\033[0;34m"; // BLUE
-    public static final String PURPLE = "\033[0;35m"; // PURPLE
-    public static final String CYAN = "\033[0;36m"; // CYAN
-    public static final String WHITE = "\033[0;37m"; // WHITE
+    public static final String BLACK = "\033[0;30m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String WHITE = "\033[0;37m";
 
-    // Bold
-    public static final String BLACK_BOLD = "\033[1;30m"; // BLACK
-    public static final String RED_BOLD = "\033[1;31m"; // RED
-    public static final String GREEN_BOLD = "\033[1;32m"; // GREEN
-    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String BLUE_BOLD = "\033[1;34m"; // BLUE
-    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
-    public static final String CYAN_BOLD = "\033[1;36m"; // CYAN
-    public static final String WHITE_BOLD = "\033[1;37m"; // WHITE
+    public static final String BLACK_BOLD = "\033[1;30m";
+    public static final String RED_BOLD = "\033[1;31m";
+    public static final String GREEN_BOLD = "\033[1;32m";
+    public static final String YELLOW_BOLD = "\033[1;33m";
+    public static final String BLUE_BOLD = "\033[1;34m";
+    public static final String PURPLE_BOLD = "\033[1;35m";
+    public static final String CYAN_BOLD = "\033[1;36m";
+    public static final String WHITE_BOLD = "\033[1;37m";
 
-    // Underline
-    public static final String BLACK_UNDERLINED = "\033[4;30m"; // BLACK
-    public static final String RED_UNDERLINED = "\033[4;31m"; // RED
-    public static final String GREEN_UNDERLINED = "\033[4;32m"; // GREEN
-    public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
-    public static final String BLUE_UNDERLINED = "\033[4;34m"; // BLUE
-    public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
-    public static final String CYAN_UNDERLINED = "\033[4;36m"; // CYAN
-    public static final String WHITE_UNDERLINED = "\033[4;37m"; // WHITE
+    public static final String BLACK_UNDERLINED = "\033[4;30m";
+    public static final String RED_UNDERLINED = "\033[4;31m";
+    public static final String GREEN_UNDERLINED = "\033[4;32m";
+    public static final String YELLOW_UNDERLINED = "\033[4;33m";
+    public static final String BLUE_UNDERLINED = "\033[4;34m";
+    public static final String PURPLE_UNDERLINED = "\033[4;35m";
+    public static final String CYAN_UNDERLINED = "\033[4;36m";
+    public static final String WHITE_UNDERLINED = "\033[4;37m";
 
-    // Background
-    public static final String BLACK_BACKGROUND = "\033[40m"; // BLACK
-    public static final String RED_BACKGROUND = "\033[41m"; // RED
-    public static final String GREEN_BACKGROUND = "\033[42m"; // GREEN
-    public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
-    public static final String BLUE_BACKGROUND = "\033[44m"; // BLUE
-    public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
-    public static final String CYAN_BACKGROUND = "\033[46m"; // CYAN
-    public static final String WHITE_BACKGROUND = "\033[47m"; // WHITE
+    public static final String BLACK_BACKGROUND = "\033[40m";
+    public static final String RED_BACKGROUND = "\033[41m";
+    public static final String GREEN_BACKGROUND = "\033[42m";
+    public static final String YELLOW_BACKGROUND = "\033[43m";
+    public static final String BLUE_BACKGROUND = "\033[44m";
+    public static final String PURPLE_BACKGROUND = "\033[45m";
+    public static final String CYAN_BACKGROUND = "\033[46m";
+    public static final String WHITE_BACKGROUND = "\033[47m";
 
-    // High Intensity
-    public static final String BLACK_BRIGHT = "\033[0;90m"; // BLACK
-    public static final String RED_BRIGHT = "\033[0;91m"; // RED
-    public static final String GREEN_BRIGHT = "\033[0;92m"; // GREEN
-    public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
-    public static final String BLUE_BRIGHT = "\033[0;94m"; // BLUE
-    public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
-    public static final String CYAN_BRIGHT = "\033[0;96m"; // CYAN
-    public static final String WHITE_BRIGHT = "\033[0;97m"; // WHITE
+    public static final String BLACK_BRIGHT = "\033[0;90m";
+    public static final String RED_BRIGHT = "\033[0;91m";
+    public static final String GREEN_BRIGHT = "\033[0;92m";
+    public static final String YELLOW_BRIGHT = "\033[0;93m";
+    public static final String BLUE_BRIGHT = "\033[0;94m";
+    public static final String PURPLE_BRIGHT = "\033[0;95m";
+    public static final String CYAN_BRIGHT = "\033[0;96m";
+    public static final String WHITE_BRIGHT = "\033[0;97m";
 
-    // Bold High Intensity
-    public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
-    public static final String RED_BOLD_BRIGHT = "\033[1;91m"; // RED
-    public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
-    public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
-    public static final String BLUE_BOLD_BRIGHT = "\033[1;94m"; // BLUE
-    public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
-    public static final String CYAN_BOLD_BRIGHT = "\033[1;96m"; // CYAN
-    public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
+    public static final String BLACK_BOLD_BRIGHT = "\033[1;90m";
+    public static final String RED_BOLD_BRIGHT = "\033[1;91m";
+    public static final String GREEN_BOLD_BRIGHT = "\033[1;92m";
+    public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";
+    public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";
+    public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";
+    public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";
+    public static final String WHITE_BOLD_BRIGHT = "\033[1;97m";
 
-    // High Intensity backgrounds
-    public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
-    public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
-    public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
-    public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
-    public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
-    public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
-    public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m"; // CYAN
-    public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m"; // WHITE
-
+    public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";
+    public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";
+    public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";
+    public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";
+    public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";
+    public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m";
+    public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";
+    public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";
 }
