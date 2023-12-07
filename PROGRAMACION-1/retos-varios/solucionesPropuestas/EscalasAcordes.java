b0de8a9b6ad85c04fef925db3a24277fe2131068
mmasias@AIO.despacho <manuel@masiasweb.com>
2023-12-07 18:07:27 +0100

    Método para mostrar las notas de un array de notas

--- PROGRAMACION-1/retos-varios/solucionesPropuestas/EscalasAcordes.java
+++ PROGRAMACION-1/retos-varios/solucionesPropuestas/EscalasAcordes.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
 
 class EscalasAcordes {
-    
-    static final String[] NOTAS = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+
+    static final String[] NOTAS = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
 
     public static void main(String[] args) {
 
         System.out.println(obtenerIndice("Do"));
         System.out.println(obtenerNota(0));
+        mostrarNotas(NOTAS);
 
+    }
+
+    private static void mostrarNotas(String[] notas) {
+        for (int nota = 0; nota < notas.length; nota++) {
+            System.out.print("[" + notas[nota] + "] / ");
+        }
+        System.out.println();
     }
 
     private static int obtenerIndice(String nota) {
