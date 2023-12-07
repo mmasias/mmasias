d8c407162688f74dbbc63fc1dfc9d40a769ec926
mmasias@AIO.despacho <manuel@masiasweb.com>
2023-12-07 18:15:10 +0100

    Construcción de la escala mayor

--- PROGRAMACION-1/retos-varios/solucionesPropuestas/EscalasAcordes.java
+++ PROGRAMACION-1/retos-varios/solucionesPropuestas/EscalasAcordes.java
@@ -6,10 +6,29 @@ class EscalasAcordes {
 
     public static void main(String[] args) {
 
-        System.out.println(obtenerIndice("Do"));
-        System.out.println(obtenerNota(0));
-        mostrarNotas(NOTAS);
+        String[] escalaMayor;
 
+        escalaMayor = escalaMayor("Do");
+        mostrarNotas(escalaMayor);
+    }
+
+    private static String[] escalaMayor(String nota) {
+        String[] patrones = { "T", "T", "S", "T", "T", "T", "S" };
+        String[] escala = new String[8];
+        int indiceNota = obtenerIndice(nota);
+
+        for (int i = 0; i < 8; i++) {
+            escala[i] = obtenerNota((indiceNota) % 12);
+            if (i < 7) {
+                if (patrones[i].equals("T")) {
+                    indiceNota += 2;
+                } else {
+                    indiceNota++;
+                }
+            }
+        }
+
+        return escala;
     }
 
     private static void mostrarNotas(String[] notas) {
